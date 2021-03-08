9eb3a0fb1696766b9a423d16b579003d94ac7842
Ceinmark <Ceinmark@DESKTOP-J0PN6TT>
2021-03-08 09:06:13 +0100

    primer cambio

---
+++ src/Programa_prueba.java
@@ -0,0 +1,10 @@
+
+public class Programa_prueba {
+
+	public static void main (String args[]) {
+		
+		System.out.println("rama master del programa");
+		System.out.println("cambios en la rama master");
+	}
+	
+}
